e7abaeaad96f8b2b2539a3fa85771496baf5e53c
KrzysztofKmiecik1975 <krzysztofkmiecik1975@gmail.com>
2021-04-07 23:33:37 +0200

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.ToIntFunction;
 
 public class Main {
@@ -13,6 +14,20 @@ public class Main {
         strings.sort(Comparator.comparingInt(toLength));
 
         System.out.println(strings);
+
+
+        System.out.println("-----------------");
+
+
+        List<User> users=Arrays.asList(new User("Ola", 12),new User("Marek", 9),new User("Beatka", 43),new User("Beatka", 46));
+
+        Comparator<User> cmpByName = (u1,u2)->u1.getName().compareTo(u2.getName());
+        Comparator<User> cmpByAge=(u1,u2)->Integer.compare(u1.getAge(),u2.getAge());
+
+        Comparator<User> byNamethenByAge = cmpByName.thenComparing(cmpByAge);
+        users.sort(byNamethenByAge);
+
+        System.out.println(users);
 
 
     }
